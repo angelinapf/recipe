0216b1a130bcf0a70fa579190809b8baf6fdb7d4
angelina <111738384+angelinapf@users.noreply.github.com>
2023-03-09 12:17:12 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+// Desktop navigation
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+// Mobile navigation
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
